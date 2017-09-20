4797199cfaf3bcdeb951b0c82193d5f0367da38d
Tejaschokshi <chokshitejas@gmail.com>
2017-09-20 14:34:51 +1000

    Creating class for intigration testing for methodes in class PaystationController

---
+++ src/src/bcccp/carpark/paystation/PaystationControllerITTest.java
@@ -0,0 +1,18 @@
+
+
+package bcccp.carpark.paystation;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Creating class for intigration testing for methodes in class PaystationController
+ * @author Tejas chokshhi
+ */
+
+ 
+}
