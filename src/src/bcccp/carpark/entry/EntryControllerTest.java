a8f2fc0495370eda614fe33f216edb9c106e72ff
Tejaschokshi <chokshitejas@gmail.com>
2017-09-20 12:19:13 +1000

    Test of buttonPushed method, of class EntryController.

--- src/src/bcccp/carpark/entry/EntryControllerTest.java
+++ src/src/bcccp/carpark/entry/EntryControllerTest.java
@@ -61,3 +61,9 @@ import static org.junit.Assert.*;
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
     }
+
+	 /**
+     * Test of buttonPushed method, of class EntryController.
+     */
+	 
+	 
