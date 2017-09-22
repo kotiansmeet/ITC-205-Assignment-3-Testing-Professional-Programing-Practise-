088fbed6a4c1632b03e958265a9c59a8b77c5511
zainubawan <30613828+zainubawan@users.noreply.github.com>
2017-09-22 22:49:07 +1000

    comments

--- src/src/bcccp/tickets/Tests_Zainab/UsageRecordFactory/UsageRecordFactory.java
+++ src/src/bcccp/tickets/Tests_Zainab/UsageRecordFactory/UsageRecordFactory.java
@@ -7,11 +7,13 @@ import org.junit.Test;
 
 public class UsageRecordFactoryTest {
 	
+	//Assigning values to variables
 	String id = "Sample Ticket ID";
 	long startTime = 1700;
 	long endTime = 1230;
 	UsageRecord uRec = new UsageRecord(id,startTime);
 	
+	//test for usagerecord
 	@Test
 	public void testUsageRecord() {
 		assertEquals(uRec.ticketId, id);
