45e12301582e2b40de372cf054bb82b415b7cee0
zainubawan <30613828+zainubawan@users.noreply.github.com>
2017-09-22 22:48:27 +1000

    Tests for UsageRecordFactory

--- src/src/bcccp/tickets/Tests_Zainab/UsageRecordFactory/UsageRecordFactory.java
+++ src/src/bcccp/tickets/Tests_Zainab/UsageRecordFactory/UsageRecordFactory.java
@@ -1,1 +1,20 @@
 
+package bcccp.tickets.season;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UsageRecordFactoryTest {
+	
+	String id = "Sample Ticket ID";
+	long startTime = 1700;
+	long endTime = 1230;
+	UsageRecord uRec = new UsageRecord(id,startTime);
+	
+	@Test
+	public void testUsageRecord() {
+		assertEquals(uRec.ticketId, id);
+	}
+	
+}
