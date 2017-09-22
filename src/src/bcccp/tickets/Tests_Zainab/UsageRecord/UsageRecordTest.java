fc802a974e6ecb977d94ba02684da97bf1cc8e6f
zainubawan <30613828+zainubawan@users.noreply.github.com>
2017-09-22 22:30:10 +1000

    Comments

--- src/src/bcccp/tickets/Tests_Zainab/UsageRecord/UsageRecordTest.java
+++ src/src/bcccp/tickets/Tests_Zainab/UsageRecord/UsageRecordTest.java
@@ -11,38 +11,45 @@ import org.junit.Test;
 public class UsageRecordTest {
 	
 	
-  //Assigning values to variables for testing
+  	//Assigning values to variables for testing
   	String id = "Sample Ticket ID";
 	long startTime = 1700;
 	long endTime = 1230;
 	UsageRecord uRec = new UsageRecord(id,startTime);
 	
+	
+	//Test for usage records
 	@Test
 	public void testUsageRecord() {
 		assertEquals(uRec.ticketId, id);
 	}
 	
+	//test for finalising time
 	@Test
 	public void testFinalise() {
 		uRec.finalise(endTime);
 		assertEquals(uRec.endDateTime, endTime);
 	}
 	
+	//test for start time
 	@Test
 	public void testGetStartTime() {
 		assertEquals(uRec.startDateTime, uRec.getStartTime());
 	}
 	
+	//test for end time
 	@Test
 	public void testGetEndTime() {
 		assertEquals(uRec.endDateTime, uRec.getEndTime());
 	}
 	
+	//test for season ticket ID validation
 	@Test
 	public void testGetSeasonTicketId() {
 		assertEquals(uRec.ticketId, uRec.getSeasonTicketId());
 	}
 	
+	//Test to check enetered string values
 	@Test
 	public void testToString() {
 		String expectedString = "Usage : startDateTime : " + startTime + ", endDateTime: " + endTime;
